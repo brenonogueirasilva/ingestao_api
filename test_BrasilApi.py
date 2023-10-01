0fcd0c9f37745c3d33851bd216bc795b6355dbc1
brenonogueirasilva <brenogueirasilva@gmail.com>
2023-10-01 20:23:00 -0300

    <test>[test_BrasilApi] : testing method execute_requests_save_file

--- test_BrasilApi.py
+++ test_BrasilApi.py
@@ -1,4 +1,4 @@
-import json
+import shutil
 import os 
 from unittest.mock import Mock , patch
 import pandas as pd
@@ -78,3 +78,29 @@ def test_generate_name_file():
     path_parameters= 'MG'
     name = brasil_api.generate_name_file(query, path_parameters) 
     assert 'path' in name and 'MG' in name and 'par1' in name and '2' in name and 'par3' in name and '4' in name
+
+@patch('requests.get')
+def test_execute_requests_save_file(mock_get):
+    '''
+    Test if the method can execute api and save json using another class
+    '''
+    requisicao = Mock()
+    requisicao.status_code = 200
+    requisicao.json = Mock(return_value= [{'chave' : 'valor'}, {'chave2' : 'valor2'}] )
+    mock_get.return_value = requisicao
+
+    download_folder = './test_download' 
+    brasil_api =  BrasilApi(
+        endpoint= "ibge/municipios/v1/",
+        query_parameters = { "par1" : 1 , "par2" : 2, "par3" : 5},
+        path_parameters= 'MG',
+        download_folder= download_folder  
+    )
+    brasil_api.execute_requests_save_file()
+    number_files = os.listdir(download_folder)
+    assert len(number_files) > 0
+    files = os.listdir(download_folder)
+    assert 'json' in files[0]
+    shutil.rmtree(download_folder)
+
+    
