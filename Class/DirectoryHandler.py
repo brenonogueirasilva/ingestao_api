464ae016345a90c4bca564c09b5b6c318a2c8040
brenonogueirasilva <brenogueirasilva@gmail.com>
2023-10-01 14:34:46 -0300

    <docs> [DirectoryHandler Class]: add annotation for class and methods

--- Class/DirectoryHandler.py
+++ Class/DirectoryHandler.py
@@ -1,16 +1,17 @@
 import os
 import json 
+import requests
 
 class DirectoryHandler:
-    def __init__(self, destination_path):
+    def __init__(self, destination_path: str):
         self.destination_path = destination_path
         if not os.path.exists(self.destination_path):
             os.mkdir(self.destination_path)
 
-    def request_to_json_file(self, object_request, name_file):
+    def request_to_json_file(self, object_request: requests.Response, name_file: str):
         path_save = f'{self.destination_path}/{name_file}.json' 
         with open(path_save, 'w') as json_file:
             json.dump(object_request.json(), json_file, indent=4)
 
-    def list_dir(self):
+    def list_dir(self) -> list:
         return os.listdir(self.destination_path)
