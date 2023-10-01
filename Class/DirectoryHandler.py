2583b3e189e1ab6a29ff50ac86d378002b81e49c
brenonogueirasilva <brenogueirasilva@gmail.com>
2023-10-01 12:46:12 -0300

    <feat>[Class Directory Handler]: create new request_to_json_file method

--- Class/DirectoryHandler.py
+++ Class/DirectoryHandler.py
@@ -1,6 +1,11 @@
 import os
+import json 
 
 class DirectoryHandler:
     def __init__(self, destination_path):
         self.destination_path = destination_path
 
+    def request_to_json_file(self, object_request, name_file):
+        path_save = f'{self.destination_path}/{name_file}.json' 
+        with open(path_save, 'w') as json_file:
+            json.dump(object_request.json(), json_file, indent=4)
