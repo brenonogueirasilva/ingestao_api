e2fcd4d3d8639d8b8ea7da5d1bff776e31285003
brenonogueirasilva <brenogueirasilva@gmail.com>
2023-10-09 00:54:19 -0300

    <refactory>[test_brasil_api.py] deletting test_generate_list_query_parameters method

--- tests/test_brasil_api.py
+++ tests/test_brasil_api.py
@@ -36,32 +36,12 @@ def test_method_request_2(mock_get):
 
     brasil_api =  BrasilApi(
         endpoint= "ibge/municipios/v1/",
-        query_parameters = { "providers" : "dados-abertos-br,gov,wikipedia"},
-        path_parameters= ['SP', 'MG' ],
-        download_folder= './test_download'  
+        query_parameter = { "providers" : "dados-abertos-br,gov,wikipedia"},
+        path_parameter= 'MG'  
     )
-    query_parameters = { "providers" : "dados-abertos-br,gov,wikipedia"}
-    path_parameter = 'SP'
-    resposta = brasil_api.request(query_parameters, path_parameter)
-    assert resposta != None 
+    response = brasil_api.request_get()
+    assert response != None 
 
-
-def test_generate_list_query_parameters():
-    '''
-    Test if the method can generate a list of dictiaries that not contain a lista, because api request do not accept lists
-    '''
-    brasil_api =  BrasilApi(
-        endpoint= "ibge/municipios/v1/",
-        query_parameters = { "par1" : [1,2,3] , "par2" : 2, "par3" : [1,2,3,4,5]},
-        path_parameters= ['SP', 'MG' ],
-        download_folder= './test_download'  
-    )
-    dict = brasil_api.generate_list_query_parameters()
-    cont_dict =0 
-    for item in dict:
-        if isinstance(item, list):
-            cont_dict += 1
-    assert cont_dict == 0
     
 def test_generate_name_file():
     '''
