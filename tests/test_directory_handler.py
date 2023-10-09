e90017fe8bc3fb3c43d200095914ec0cfdcafe0b
brenonogueirasilva <brenogueirasilva@gmail.com>
2023-10-09 01:07:26 -0300

    <feat>[test_directory_handler.py] new method to test  test_json_envelope_to_dataframe

--- tests/test_directory_handler.py
+++ tests/test_directory_handler.py
@@ -120,6 +120,37 @@ def test_json_to_dataframe():
             df = directory_handler.json_to_dataframe(path)
             assert isinstance(df, pd.DataFrame) 
 
-    
+def test_json_envelope_to_dataframe():
+    '''
+    Test if json turns into dataframe
+    '''
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        folder = "download_folder"
+        temp_folder = rf"{tmpdirname}\{folder}"
+
+        dict_json = {
+    "envelope": {
+        "endpoint": "ibge/municipios/v1/",
+        "path": "BA",
+        "providers": "dados-abertos-br,gov,wikipedia"
+    },
+    "content": [
+        {
+            "nome": "ABAIRA",
+            "codigo_ibge": "2900108"
+        }] }
+
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            folder = "download_folder"
+            temp_dir = str(tmpdirname).replace('\\' , '/')
+            temp_dir = f"{temp_dir}/{folder}/"
+
+            directory_handler =DirectoryHandler(temp_dir)
+            with open(temp_dir + 'ibge_municipios_path(MG).json' , 'w') as json_file:
+                    json.dump( dict_json, json_file, indent=4)
+
+            path = f"{temp_dir}ibge_municipios_path(MG).json"
+            df = directory_handler.json_envelope_to_dataframe(path)
+            assert isinstance(df, pd.DataFrame)    
 
 
